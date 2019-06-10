100fa9626ad0bbeb36055f24e6959912bb3e054f
Dymensi0n <Dymensi0nArt@gmail.com>
2019-06-09 21:01:34 -0700

    Matches work and card flipping works as well as Moves count

--- js/app.js
+++ js/app.js
@@ -24,20 +24,18 @@ function shuffle(array) {
 
     return array;
 }
-
-
-//Variables
-
+//List of cards in an Array
 let cardList = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o',
                     'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt',
                     'fa-cube', 'fa-cube', 'fa-leaf', 'fa-leaf',
                     'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb'];
 
+                    //Generates the cards properties
 function generateCards(card){
-    return `<li class="card" card-data="${card}"><i class="fa ${card}"></i></li>`;
+    return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
 }
                     
-//Generate cards 
+//Starts a new game or initialization 
 function newGame(){
     
     let cardDeck = document.querySelector('.deck');
@@ -50,13 +48,16 @@ function newGame(){
     cardDeck.innerHTML = cardHTML.join('');
 }
 
+//Run the game
 newGame();
 
 //Grab all cards with class '.card'
 const allCards = document.querySelectorAll('.card');
 let openCards = [];
+let playerMoves = document.querySelector('.moves');
+let totalMatches = 0;
 
-//flips cards to close them
+//Hides or closes cards function
 function closeCards() {
     setTimeout(function() {
         openCards.forEach(function(card) {
@@ -64,24 +65,31 @@ function closeCards() {
             console.log('array number ' + openCards.length);
         });
         openCards = [];
-    }, 1000);   
+    }, 500);   
+}
+
+function playerWin() {
+    console.log('YOU WON!!!');
+    alert('Congratulations, you found all matching pairs!');
 }
 
 //Flips card and shows
     allCards.forEach(function(card) {
         card.addEventListener('click', function(e){
 
-            // If cards are open or swoing they will not do anything
+            // If cards are open or showing they will not do anything
             if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
                 
                 openCards.push(card);
                 card.classList.add('open', 'show');
+                playerMoves.innerText++;
+                console.log(playerMoves);
                 
                 
                 // If more than 2 cards are showing, hide them
                 if (openCards.length == 2) {
-                    if (openCards[0].dataset.card == openCards[1].dataset.card) {
-                        console.log('You found a Macth!');
+                    if (openCards[0].dataset.card == openCards[1].dataset.card) { //This looks for the data property "data-card" from the html
+                        console.log('Match!');
                         openCards[0].classList.add('match');
                         openCards[0].classList.add('open');
                         openCards[0].classList.add('show');
@@ -90,11 +98,18 @@ function closeCards() {
                         openCards[1].classList.add('open');
                         openCards[1].classList.add('show');
 
+                        totalMatches++;
+                        console.log(totalMatches);
+
                         openCards = [];
+
+                        if (totalMatches == 8) {
+                            playerWin();
+                        }
 
                     } else {
 
-                    //Hide Cards
+                    //Hide Cards running this function
                     closeCards();
                 }
             }
